efdd8e77f3265c731465b88a7120af699b210be5
vinodkottem <vinodkottem@gmail.com>
2017-07-03 02:41:19 +0530

    okay

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,14 +5,14 @@ module.exports = {
   }],
     deploy: {
         production: {
-            user: 'ubuntu',
-            host: 'ec2-54-201-127-201.us-west-2.compute.amazonaws.com',
-            key: '~/.ssh/ubuntuawskey.pem',
-            ref: 'origin/master',
-            repo: 'git@github.com:vinodkottem/aws-nginx-node.git',
-            path: '/home/ubuntu/awsnginxnode',
-            restart: 'pm2 startOrRestart ecosystem.config.js',
-            deploy: 'pm2 deploy ecosystem.config.js production',
+            'user': 'ubuntu',
+            'host': 'ec2-54-201-127-201.us-west-2.compute.amazonaws.com',
+            'key': '~/.ssh/ubuntuawskey.pem',
+            'ref': 'origin/master',
+            'repo': 'git@github.com:vinodkottem/aws-nginx-node.git',
+            'path': '/home/ubuntu/awsnginxnode',
+            'restart': 'pm2 startOrRestart ecosystem.config.js',
+            'deploy': 'pm2 deploy ecosystem.config.js production',
             'post-deploy': 'npm install && npm run-script restart'
         }
     }
