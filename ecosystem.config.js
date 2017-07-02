ec3c4e7c97a6f3fb1d09a4f207fa785eeb51e68d
vinodkottem <vinodkottem@users.noreply.github.com>
2017-07-03 02:16:46 +0530

    config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    apps: [{
+        name: 'awsnginxnode',
+        script: './server.js'
+  }],
+    deploy: {
+        production: {
+            user: 'ubuntu',
+            host: 'ec2-54-201-127-201.us-west-2.compute.amazonaws.com',
+            key: '~/.ssh/ubuntuawskey.pem',
+            ref: 'origin/master',
+            repo: 'git@github.com:vinodkottem/aws-nginx-node.git',
+            path: '/home/ubuntu/awsnginxnode',
+            'post-deploy': 'npm install && npm run-script restart'
+        }
+    }
+}
