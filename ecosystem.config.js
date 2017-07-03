971d3f0996aca40cdbb2d9a182daf485cc83d04d
vinodkottem <vinodkottem@gmail.com>
2017-07-03 13:28:49 +0530

    route chnaged

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
     deploy: {
         production: {
             'user': 'ubuntu',
-            'host': 'ec2-54-201-127-201.us-west-2.compute.amazonaws.com',
+            'host': 'ec2-34-211-50-208.us-west-2.compute.amazonaws.com',
             'key': '~/.ssh/ubuntuawskey.pem',
             'ref': 'origin/master',
             'repo': 'git@github.com:vinodkottem/aws-nginx-node.git',
